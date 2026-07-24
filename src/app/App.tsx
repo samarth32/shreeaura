66e0c5a1a0a140f4dfcd3a28b013ba74a2c9f152
samarth32 <98467014+samarth32@users.noreply.github.com>
2026-07-24 14:31:49 +0530

    Add files via upload

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,6 @@
 import { RouterProvider } from 'react-router';
-import { router } from './routes'; 
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Products from "./pages/Products";
-import NotFound from "./pages/NotFound";
+import { router } from './routes';
 
 export default function App() {
   return <RouterProvider router={router} />;
-} 
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
+}
