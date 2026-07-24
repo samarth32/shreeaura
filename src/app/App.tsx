939072208a8604bd718758a106691ba158b30035
samarth32 <98467014+samarth32@users.noreply.github.com>
2026-07-24 14:19:07 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,29 @@
 import { RouterProvider } from 'react-router';
-import { router } from './routes';
+import { router } from './routes'; 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return <RouterProvider router={router} />;
-}
+} 
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
