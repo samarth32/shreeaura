3f1a2742b0b0e34e0f8e9cd1de0ef9b717554743
samarth32 <98467014+samarth32@users.noreply.github.com>
2026-07-24 13:59:19 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@
 
   // createRoot(document.getElementById("root")!).render(<App />); 
 
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";   // or "./app/App" depending on your project
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
