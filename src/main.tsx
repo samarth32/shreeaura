934c8dcd4188238eea75f3be73601079afa6cf12
samarth32 <98467014+samarth32@users.noreply.github.com>
2026-07-24 14:31:27 +0530

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,5 @@
-  // import { createRoot } from "react-dom/client";
-  // import App from "./app/App.tsx";
-  // import "./styles/index.css";
+  import { createRoot } from "react-dom/client";
+  import App from "./app/App.tsx";
+  import "./styles/index.css";
 
-  // createRoot(document.getElementById("root")!).render(<App />); 
-
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";   // or "./app/App" depending on your project
-import "./styles/index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  {/*<BrowserRouter> */}
-    <App />
-  {/* </BrowserRouter> */}
-);
+  createRoot(document.getElementById("root")!).render(<App />);
