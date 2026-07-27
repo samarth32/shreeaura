16c23b13cd2e9e26c44f8ff99fe4d09601dc78e1
samarth32 <98467014+samarth32@users.noreply.github.com>
2026-07-27 21:20:16 +0530

    Update Root.tsx

--- src/app/components/Root.tsx
+++ src/app/components/Root.tsx
@@ -1,12 +1,28 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { useEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function Root() {
+  const location = useLocation();
+
   useEffect(() => {
     document.documentElement.classList.add('dark');
   }, []);
+
+  useEffect(() => {
+    const path = location.pathname;
+    const pathParts = path.split("/").filter(Boolean);
+    
+    if (path === "/") {
+      document.title = "ShreeAura | Home";
+    } else if (pathParts.length > 0) {
+      const pageName = pathParts[0].charAt(0).toUpperCase() + pathParts[0].slice(1);
+      document.title = `ShreeAura | ${pageName}`;
+    } else {
+      document.title = "ShreeAura | Official Website";
+    }
+  }, [location.pathname]);
 
   return (
     <div className="min-h-screen bg-background text-foreground">
